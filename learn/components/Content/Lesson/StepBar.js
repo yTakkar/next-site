483f0c7d67756b154352510f50015f4fe46d434f
Michael Fix <mike@dawnlabs.io>
2019-02-11 16:03:48 -0800

    improve learn points bar styles (#155)
    * improve learn points bar styles
    
    * remove old onClick

--- learn/components/Content/Lesson/StepBar.js
+++ learn/components/Content/Lesson/StepBar.js
@@ -1,99 +1,82 @@
 import React from 'react'
-import Link from 'next/link'
 import Router from 'next/router'
 import PropTypes from 'prop-types'
 import classNames from 'classnames'
 
 import Button from '../../new/button'
-import CheckIcon from '../../icons/check'
-
-const styles = {
-  bold: {
-    fontWeight: 800
-  }
-}
 
 class Step extends React.Component {
-  changeRoute() {
-    const { href, as } = this.props
-    if (!href) return
-
-    Router.push(href, as)
-  }
-
-  render () {
+  render() {
     const { disabled, selected, name, href, as } = this.props
 
-    return <div>
-      <Button
-        className={classNames('step-btn', { 'step-selected': selected })}
-        onClick={() => this.changeRoute()}
-        href={href ? href : undefined}
-        as={as}
-        disabled={disabled}
-        invert={selected}
-        light
-      >
-        {name}
-      </Button>
-      <style jsx global>{`
-        .step-btn.step-selected {
-          font-weight: 600;
-        }
-        .step-btn.disabled {
-          color: #ccc;
-          cursor: not-allowed;
-        }
-      `}</style>
-      <style jsx>{`
-        div {
-          margin-right: 1rem;
-          display: inline-block;
-        }
-      `}</style>
-    </div>
+    return (
+      <div>
+        <Button
+          className={classNames('step-btn', { 'step-selected': selected })}
+          href={href ? href : undefined}
+          as={as}
+          disabled={disabled}
+          invert={selected}
+          light
+        >
+          {name}
+        </Button>
+        <style jsx global>{`
+          .step-btn.step-selected {
+            font-weight: 600;
+          }
+          .step-btn.disabled {
+            color: #ccc;
+            cursor: not-allowed;
+          }
+        `}</style>
+        <style jsx>{`
+          div {
+            margin-right: 1rem;
+            display: inline-block;
+          }
+        `}</style>
+      </div>
+    )
   }
 }
 
 class StepBar extends React.Component {
-  renderStep (step, index) {
+  renderStep(step, index) {
     const { courseId, lessonId, currentStepId } = this.props
 
+    const selected = step.id === currentStepId
+    const name = step.points + (selected ? ' points' : '')
+
     if (!step.visited) {
-      return (
-        <Step
-          key={index}
-          name={step.points}
-          disabled
-        />
-      )
+      return <Step key={index} name={name} disabled />
     }
 
     return (
       <Step
         key={index}
-        selected={step.id === currentStepId}
+        selected={selected}
         href={`/learn/content?course=${courseId}&lesson=${lessonId}&step=${step.id}`}
         as={`/learn/${courseId}/${lessonId}/${step.id}`}
-        name={step.points}
+        name={name}
       />
     )
   }
 
-  render () {
+  render() {
     const { steps, courseId, lessonId, currentStepId } = this.props
 
     return (
       <div>
-        {
-          // hide when not start
-          steps.length ? <Step
+        {// hide when not start
+        steps.length ? (
+          <Step
             selected={!currentStepId}
             href={`/learn/content?course=${courseId}&lesson=${lessonId}`}
             as={`/learn/${courseId}/${lessonId}`}
             name="Introduction"
-          /> : null
-        }
+          />
+        ) : null}
         {steps.map((s, i) => this.renderStep(s, i))}
       </div>
     )
@@ -107,7 +90,8 @@ StepBar.propTypes = {
   currentStepId: PropTypes.string
 }
 
-StepBar.fragment = (c) => c.createFragment(`
+StepBar.fragment = c =>
+  c.createFragment(`
   fragment on Step {
     id,
     points,

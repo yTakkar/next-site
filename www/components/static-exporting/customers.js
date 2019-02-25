312f697b5b76f456c0d7f578c752cfe060c826e5
Michael Fix <mike@dawnlabs.io>
2019-02-25 04:00:54 -0800

    New static exporting customers (#184)
    @timneutkens https://sapien.network did not have a good SVG to use as an icon, and in-my-opinion the one I am currently using does not look good enough to include — thus this PR is WIP. 
    
    Adds the following to Static Exporting carousel: Verge Currency, NuCypher, Rossman.pl, Sapien, Audiense, and Motive

--- www/components/static-exporting/customers.js
+++ www/components/static-exporting/customers.js
@@ -7,44 +7,79 @@ import Expo from '../icons/companies/expo';
 import Blockstack from '../icons/companies/blockstack';
 import Material from '../icons/companies/material-ui';
 import Plotly from '../icons/companies/plotly';
+import VergeCurrency from '../icons/companies/vergecurrency';
+import NuCypher from '../icons/companies/nucypher';
+import Rossmann from '../icons/companies/rossmann';
+import Audiense from '../icons/companies/audiense';
+import Motiv from '../icons/companies/motiv';
+
+const slides = [
+  {
+    href: 'https://blockstack.org/',
+    image: '/static/images/showcases/blockstack.png',
+    alt: 'Blockstack',
+    logo: <Blockstack />
+  },
+  {
+    href: 'https://plot.ly/',
+    image: '/static/images/showcases/plotly.jpg',
+    alt: 'Plotly',
+    logo: <Plotly />
+  },
+  {
+    href: 'https://www.rossmann.pl/',
+    image: '/static/images/showcases/rossmann.png',
+    alt: 'Rossmann',
+    logo: <Rossmann />
+  },
+  {
+    href: 'https://audiense.com/',
+    image: '/static/images/showcases/audiense.png',
+    alt: 'Audiense',
+    logo: <Audiense />
+  },
+  {
+    href: 'https://mymotiv.com/',
+    image: '/static/images/showcases/motiv.png',
+    alt: 'Motiv',
+    logo: <Motiv />
+  },
+  {
+    href: 'https://www.nucypher.com/',
+    image: '/static/images/showcases/nucypher.png',
+    alt: 'NuCypher',
+    logo: <NuCypher />
+  },
+  {
+    href: 'https://material-ui.com/',
+    image: '/static/images/showcases/material-ui.jpg',
+    alt: 'Material UI',
+    logo: <Material />
+  },
+  {
+    href: 'https://vergecurrency.com/',
+    image: '/static/images/showcases/verge.jpg',
+    alt: 'Verge Currency',
+    logo: <VergeCurrency />
+  },
+  {
+    href: 'https://expo.io/',
+    image: '/static/images/showcases/showcases-13.jpg',
+    alt: 'Expo',
+    logo: <Expo />
+  },
+  {
+    href: 'https://blockchain.com/',
+    image: '/static/images/showcases/blockchain.png',
+    alt: 'Blockchain',
+    logo: <Blockchain />
+  }
+];
 
 export default () => (
   <Container padding wide>
     <SectionHeader id="customers" title="Who's Using Static Exports" />
-    <Carousel
-      slides={[
-        {
-          href: 'https://expo.io/',
-          image: '/static/images/showcases/showcases-13.jpg',
-          alt: 'Expo',
-          logo: <Expo />
-        },
-        {
-          href: 'https://blockchain.com/',
-          image: '/static/images/showcases/blockchain.png',
-          alt: 'Blockchain',
-          logo: <Blockchain />
-        },
-        {
-          href: 'https://plot.ly/',
-          image: '/static/images/showcases/plotly.jpg',
-          alt: 'Plotly',
-          logo: <Plotly />
-        },
-        {
-          href: 'https://material-ui.com/',
-          image: '/static/images/showcases/material-ui.jpg',
-          alt: 'Material UI',
-          logo: <Material />
-        },
-        {
-          href: 'https://blockstack.org/',
-          image: '/static/images/showcases/blockstack.png',
-          alt: 'Blockstack',
-          logo: <Blockstack />
-        }
-      ]}
-    />
+    <Carousel slides={slides} />
     <style jsx>
       {`
         h4 {

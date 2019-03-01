2bfdf081f816d8bd3cfa1628e219681b95fab8a7
Michael Fix <mike@dawnlabs.io>
2019-03-01 08:51:01 -0800

    Fix react-spring in Chrome 41 with polyfills (#195)
    You can test it here: https://www.browserling.com/browse/win/7/chrome/41/https%3A%2F%2Fnext-site-lxhvonifn.now.sh
    
    Each of these methods are used in React Spring: https://unpkg.com/react-spring

--- www/lib/polyfill.js
+++ www/lib/polyfill.js
@@ -1,3 +1,10 @@
+import 'core-js/fn/array/find';
+import 'core-js/fn/array/find-index';
+import 'core-js/fn/array/from';
+import 'core-js/fn/object/entries';
+import 'core-js/fn/object/values';
+import 'core-js/fn/symbol/iterator';
+
 if (!Object.entries) {
   Object.entries = function(obj) {
     var ownProps = Object.keys(obj),

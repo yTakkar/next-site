5436b260570a085f994dc9c66dc5086e2a343f7f
Anton Harniakou <anton.harniakou@gmail.com>
2019-03-03 01:15:29 +0300

    <Button /> uses button element if there's no href prop provided (#199)
    This change provides several improvements:
    
    - buttons are focusable, contrary to `<a></a>` element without href;
    - buttons are pressable with Enter/Spacebar
    - elements are announced as they should be

--- www/components/button.js
+++ www/components/button.js
@@ -1,20 +1,11 @@
 import Link from 'next/link';
 import classNames from 'classnames';
+import css from 'styled-jsx/css';
 
 import withPure from './hoc/pure';
 
-export default withPure(
-  ({ children, invert, href, as, className, prefetch, ...props }) => {
-    const a = (
-      <a
-        className={classNames(className, 'fw4 no-drag', { invert })}
-        role="button"
-        {...props}
-      >
-        {children}
-        <style jsx>
-          {`
-            a {
+const cachedStyles = css`
+            .btn {
               display: inline-block;
               cursor: pointer;
               text-decoration: none;
@@ -22,42 +13,66 @@ export default withPure(
               margin: -0.25rem -0.5rem;
               border-radius: 7px;
               color: #0076ff;
+              background-color: transparent;
+              border: none;
+              font-size: inherit;
+              line-height: inherit;
               transition: background 0.2s ease, color 0.2s ease,
                 box-shadow 0.2s ease;
             }
-            a:hover {
+            .btn:hover {
               color: #0076ff;
               background: rgba(0, 118, 255, 0.1);
             }
-            a.invert {
+            .btn.invert {
               margin: 0;
               padding: 0 2rem;
               height: 2.5rem;
               line-height: 2.5rem;
               border-radius: 7px;
-              background: #0076ff;
+              background-color: #0076ff;
               box-shadow: 0 4px 14px 0 rgba(0, 118, 255, 0.39);
               color: white;
             }
-            a.invert:hover {
+            .btn.invert:hover {
               background: rgba(0, 118, 255, 0.9);
               box-shadow: 0 6px 20px rgba(0, 118, 255, 0.23);
             }
-            a.invert:active {
+            .btn.invert:active {
               background: #006ae6;
             }
-          `}
+          `;
+export default withPure(
+  function Button({ children, invert, href, as, className, prefetch, ...props }) {
+    const cachedClassNames = classNames(className, 'btn', 'fw4 no-drag', { invert });
+    const a = (
+      <a
+        className={cachedClassNames}
+        {...props}
+      >
+        {children}
+        <style jsx>
+          {cachedStyles}
         </style>
       </a>
     );
 
-    if (href) {
-      return (
-        <Link href={href} as={as} prefetch={prefetch}>
-          {a}
-        </Link>
-      );
-    }
-    return a;
+    const btn = (
+      <button
+        className={cachedClassNames}
+        {...props}
+      >
+        {children}
+        <style jsx>
+          {cachedStyles}
+        </style>
+      </button>
+    );
+    return (
+      href ?
+      <Link href={href} as={as} prefetch={prefetch}>
+        {a}
+      </Link> : btn
+    );
   }
 );

efe43d7868b4f663a113814a52379bd7cc785859
Michael Fix <mike@dawnlabs.io>
2019-03-08 12:41:28 -0800

    add feature paragraphs to lander (#223)

--- www/components/home/features.js
+++ www/components/home/features.js
@@ -1,49 +1,99 @@
-import Container from '../container'
-import Button from '../button'
-import SectionHeader from '../section-header'
-import CompanySlider from './company-slider'
+import Container from '../container';
+import Button from '../button';
+import SectionHeader from '../section-header';
+import CompanySlider from './company-slider';
 
 export default function Features() {
-  return <Container wide role='region' aria-labelledby='features'>
-    <Container center padding>
-      <SectionHeader id='features' title='Why Next.js' description='The world’s leading companies use and love Next.js' />
-      <div className="row">
-        <div className="column">
-          <h3 className="f3 fw6">Zero Setup</h3>
-          <p>Automatic code splitting, filesystem based routing, hot code reloading and universal rendering.</p>
-          <Button href="/learn">Learn Next.js</Button>
+  return (
+    <Container wide role="region" aria-labelledby="features">
+      <Container center padding>
+        <SectionHeader
+          id="features"
+          title="Why Next.js"
+          description="The world’s leading companies use and love Next.js"
+        />
+        <div className="row">
+          <div className="column">
+            <h3 className="f3 fw6">
+              <Button href="/features/server-side-rendering">
+                Server Rendering
+              </Button>
+            </h3>
+            <p>
+              With Next.js, server rendering React applications has never been
+              easier, no matter where your data is coming from.
+            </p>
+          </div>
+          <div className="column">
+            <h3 className="f3 fw6">
+              <Button href="/features/static-exporting">
+                Static Exporting
+              </Button>
+            </h3>
+            <p>
+              No need to learn a new framework. Exporting a static site with
+              Next.js is as easy as a single command.
+            </p>
+          </div>
+          <div className="column">
+            <h3 className="f3 fw6">
+              <Button href="/features/css-in-js">CSS-in-JS</Button>
+            </h3>
+            <p>
+              Next.js comes with <code>styled-jsx</code> included, but it also
+              works with every CSS-in-JS solution you know and love.
+            </p>
+          </div>
         </div>
-        <div className="column">
-          <h3 className="f3 fw6">Fully Extensible</h3>
-          <p>Complete control over Babel and Webpack. Customizable server, routing and next-plugins.</p>
-          <Button href="/docs">View Full Documentation</Button>
+        <div className="row">
+          <div className="column">
+            <h3 className="f3 fw6">Zero Setup</h3>
+            <p>
+              Automatic code splitting, filesystem based routing, hot code
+              reloading and universal rendering.
+            </p>
+            <Button href="/learn">Learn Next.js</Button>
+          </div>
+          <div className="column">
+            <h3 className="f3 fw6">Fully Extensible</h3>
+            <p>
+              Complete control over Babel and Webpack. Customizable server,
+              routing and next-plugins.
+            </p>
+            <Button href="/docs">View Full Documentation</Button>
+          </div>
+          <div className="column">
+            <h3 className="f3 fw6">Ready for Production</h3>
+            <p>
+              Optimized for a smaller build size, faster dev compilation and
+              dozens of other improvements.
+            </p>
+            <Button href="/showcase">View Showcase</Button>
+          </div>
         </div>
-        <div className="column">
-          <h3 className="f3 fw6">Ready for Production</h3>
-          <p>Optimized for a smaller build size, faster dev compilation and dozens of other improvements.</p>
-          <Button href="/showcase">View Showcase</Button>
-        </div>
-      </div>
-      <style jsx>{`
-      .column {
-        text-align: left;
-      }
-      // CSS only media query for tablet
-      @media screen and (max-width: 960px) {
-        .row {
-          flex-direction: column;
-          margin: -1.5rem 0;
-        }
-        .column {
-          width: 100%;
-          padding: 1.5rem 0;
-          text-align: center;
-          max-width: 350px;
-        }
-      }
-    `}</style>
+        <style jsx>{`
+          .row:not(:last-of-type) {
+            margin-bottom: 3rem;
+          }
+          .column {
+            text-align: left;
+          }
+          // CSS only media query for tablet
+          @media screen and (max-width: 960px) {
+            .row {
+              flex-direction: column;
+              margin: -1.5rem 0;
+            }
+            .column {
+              width: 100%;
+              padding: 1.5rem 0;
+              text-align: center;
+              max-width: 350px;
+            }
+          }
+        `}</style>
+      </Container>
+      <CompanySlider />
     </Container>
-    <CompanySlider />
-  </Container>
+  );
 }
-

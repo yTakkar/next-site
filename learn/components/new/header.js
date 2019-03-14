220cfd4892ecfbff6e6cdba2f8c4baca9c692011
Tim Neutkens <tim@timneutkens.nl>
2019-03-14 20:20:52 +0100

    Update page, navbar, header (#235)

--- learn/components/new/header.js
+++ learn/components/new/header.js
@@ -1,77 +1,102 @@
-import React, { PureComponent } from 'react'
+import React, { PureComponent } from 'react';
+import classNames from 'classnames';
 
-export default class extends PureComponent {
-  state = {
-    scrolled: false,
-    fixed: false,
-    active: false
-  }
-  onScroll = () => {
-    let scroll = window.scrollY || document.body.scrollTop
-    let scrolled = scroll > (this.props.distance || 0)
-    let fixed = scroll >= (this.props.distance || 0)
-    let active = scroll >= (this.props.active || 0)
-
-    if (
-      scrolled !== this.state.scrolled ||
-      fixed !== this.state.fixed ||
-      active !== this.state.active
-    ) {
-      this.setState({ scrolled, fixed, active })
-    }
-  }
-  componentDidMount() {
-    window.addEventListener('scroll', this.onScroll)
-    this.onScroll()
-  }
-  componentWillUnmount() {
-    window.removeEventListener('scroll', this.onScroll)
-  }
+export default class Header extends PureComponent {
   render() {
-    const { scrolled, fixed, active } = this.state
-    const { height, offset, shadow, zIndex, background, defaultActive, children } = this.props
+    const {
+      height,
+      shadow,
+      zIndex,
+      background,
+      defaultActive,
+      dotBackground,
+      children
+    } = this.props;
+
+    const desktopHeight = height.desktop || Number(height) || 0;
+    const mobileHeight = height.mobile || desktopHeight;
+    const tabletHeight = height.tablet || desktopHeight;
+
+    const desktopShadow =
+      shadow.desktop || (typeof shadow === 'boolean' ? shadow : false);
+    const tabletShadow =
+      shadow.tablet || (typeof shadow === 'boolean' ? shadow : false);
+    const mobileShadow =
+      shadow.mobile || (typeof shadow === 'boolean' ? shadow : false);
 
     return (
       <header>
-        <style jsx>{`
-          header {
-            left: 0;
-            top: 0;
-            width: 100%;
-            height: ${height}px;
-          }
-          .fixed-container {
-            position: relative;
-            width: 100%;
-            display: flex;
-            flex-direction: column;
-            justify-content: space-around;
-            align-items: center;
-            left: 0;
-            z-index: ${zIndex || 1000};
-            background: ${background || 'rgba(255, 255, 255, 0.98)'};
-            transition: box-shadow 0.5s ease;
-          }
-          .fixed {
-            position: fixed;
-            top: ${offset || 0}px;
-          }
-          .scrolled {
-            position: fixed;
-            top: ${offset || 0}px;
-          }
-          .active {
-            ${shadow ? `box-shadow: 0px 6px 20px rgba(0, 0, 0, 0.06);` : ''}
-          }
-        `}</style>
         <div
-          className={`fixed-container${scrolled ? ' scrolled' : ''}${fixed ? ' fixed' : ''}${
-            active || defaultActive ? ' active' : ''
-          }`}
+          className={classNames('fixed-container active', {
+            'show-logo': dotBackground
+          })}
         >
           {children}
         </div>
+        <style jsx>
+          {`
+            header {
+              left: 0;
+              width: 100%;
+              height: ${desktopHeight}px;
+              position: -webkit-sticky;
+              position: sticky;
+              top: ${defaultActive ? 0 : -desktopHeight}px;
+              z-index: 1000;
+            }
+            @media screen and (max-width: 960px) {
+              header {
+                height: ${tabletHeight}px;
+                top: ${defaultActive ? 0 : -tabletHeight}px;
+              }
+            }
+            @media screen and (max-width: 640px) {
+              header {
+                height: ${mobileHeight}px;
+                top: ${defaultActive ? 0 : -mobileHeight}px;
+              }
+            }
+            .fixed-container {
+              position: relative;
+              display: flex;
+              flex-direction: column;
+              justify-content: space-around;
+              align-items: center;
+              width: 100%;
+              left: 0;
+              z-index: ${zIndex || 1000};
+              transition: box-shadow 0.5s ease, background 0.2s ease;
+              ${dotBackground
+                ? `
+                background-image: radial-gradient(circle, #D7D7D7, #D7D7D7 1px, #FFF 1px, #FFF);
+                background-size: 28px 28px;
+              `
+                : 'background: rgba(255, 255, 255, 0);'};
+            }
+            .active {
+              background: ${background || 'rgba(255, 255, 255, 0.98)'};
+              box-shadow: ${desktopShadow
+                ? '0px 6px 20px rgba(0, 0, 0, 0.06)'
+                : 'unset'};
+              pointer-events: auto;
+            }
+            @media screen and (max-width: 960px) {
+              .active {
+                box-shadow: ${tabletShadow
+                  ? '0px 6px 20px rgba(0, 0, 0, 0.06)'
+                  : 'unset'};
+              }
+            }
+            @media screen and (max-width: 640px) {
+              .active {
+                box-shadow: ${mobileShadow
+                  ? '0px 6px 20px rgba(0, 0, 0, 0.06)'
+                  : 'unset'};
+              }
+            }
+          `}
+        </style>
       </header>
-    )
+    );
   }
 }

401d8dbadb3bd66fdefed3e50f461295ad86a244
JJ Kasper <22380829+ijjk@users.noreply.github.com>
2019-03-21 23:12:38 -0500

    Update customers and showcase preview for AMP (#258)

--- www/components/home/customers.js
+++ www/components/home/customers.js
@@ -2,57 +2,29 @@ import Container from '../container';
 import Button from '../button';
 import SectionHeader from '../section-header';
 import ShowcasePreview from './showcase-preview';
-import IObserver from '../intersection-observer';
 
-export default class Customers extends React.PureComponent {
-  state = {
-    loadPreview: false
-  };
-
-  handleIntersect = entry => {
-    if (entry.intersectionRatio > 0) {
-      this.setState({ loadPreview: true });
-    }
-  };
-
-  render() {
-    return (
-      <IObserver
-        once={true}
-        rootMargin="50%"
-        onIntersect={this.handleIntersect}
-        render={({ innerRef }) => (
-          <Container
-            gray
-            wide
-            center
-            padding
-            role="region"
-            aria-labelledby="customers"
-          >
-            <SectionHeader
-              innerRef={innerRef}
-              anchor="showcases"
-              id="customers"
-              title="Who’s Using Next.js"
-              description="We’re honored some of the most talented creatives out there build with Next.js"
-            />
-            {this.state.loadPreview && <ShowcasePreview />}
-            <div>
-              <style jsx>{`
-                div {
-                  z-index: 1;
-                  position: relative;
-                  margin-top: 2rem;
-                }
-              `}</style>
-              <Button href="/showcase" invert>
-                View Gallery
-              </Button>
-            </div>
-          </Container>
-        )}
+export default function Customers() {
+  return (
+    <Container gray wide center padding role="region" aria-labelledby="customers">
+      <SectionHeader
+        anchor="showcases"
+        id="customers"
+        title="Who’s Using Next.js"
+        description="We’re honored some of the most talented creatives out there build with Next.js"
       />
-    );
-  }
+      <ShowcasePreview />
+      <div>
+        <style jsx>{`
+          div {
+            z-index: 3;
+            position: relative;
+            margin-top: 2rem;
+          }
+        `}</style>
+        <Button href="/showcase" invert>
+          View Gallery
+        </Button>
+      </div>
+    </Container>
+  );
 }

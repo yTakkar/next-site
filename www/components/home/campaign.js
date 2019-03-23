c36d3d2f184bf15213149bc047a66094e664ae29
Joe Haddad <timer150@gmail.com>
2019-03-23 08:35:53 -0400

    CSS-only word slider (#262)
    * Replace JS word slider with CSS version
    
    * Set animation timing function to be smoother
    
    * Tweak campaign per @quietshu

--- www/components/home/campaign.js
+++ www/components/home/campaign.js
@@ -1,28 +1,88 @@
-import Container from '../container';
-import WordSlider from '../word-slider';
+const campaignWords = [
+  'Production',
+  'Server-Rendered Apps',
+  'Static Websites',
+  'the Enterprise',
+  'the Desktop',
+  'the Mobile Web',
+  'Lightweight Apps',
+  'SEO-Friendly Sites',
+  'PWAs',
+  'Electron'
+];
 
-export default () => (
-  <div className="slider-container">
-    <WordSlider duration={1800}>
-      <span>Production</span>
-      <span>Server-Rendered Apps</span>
-      <span>Static Websites</span>
-      <span>the Enterprise</span>
-      <span>the Desktop</span>
-      <span>the Mobile Web</span>
-      <span>Lightweight Apps</span>
-      <span>SEO-Friendly Sites</span>
-      <span>PWAs</span>
-      <span>Electron</span>
-    </WordSlider>
-    <style jsx>{`
-      .slider-container {
-        margin: auto;
-        margin-top: 0;
-        margin-bottom: -1rem;
-        line-height: 1.4em;
-        white-space: nowrap;
-      }
-    `}</style>
-  </div>
-);
+export default () => {
+  const animationDuration = 1.8;
+  const animationLength = animationDuration * campaignWords.length;
+  const endOfAnimation = 100 / campaignWords.length;
+  const animationOverlap = 0.1;
+
+  return (
+    <div className="slider-container">
+      <div className="words">
+        {campaignWords.map((word, index) => (
+          <span
+            key={word}
+            style={{
+              animationDelay:
+                index === 0 ? '1ms' : `${animationDuration * index}s`
+            }}
+          >
+            {word}
+          </span>
+        ))}
+      </div>
+      <style jsx>{`
+        .slider-container {
+          margin: auto;
+          margin-top: 0;
+          margin-bottom: -1rem;
+          line-height: 1.4em;
+          white-space: nowrap;
+        }
+
+        .words {
+          width: 100%;
+          height: 3.4em;
+          display: block;
+          margin-top: 1rem;
+          margin-bottom: 2rem;
+        }
+
+        .words span {
+          position: absolute;
+          opacity: 0;
+          overflow: hidden;
+          animation: slide-word ${animationLength}s linear infinite 0s;
+          animation-timing-function: cubic-bezier(0.19, 0.82, 0.84, 1.06);
+        }
+
+        @keyframes slide-word {
+          0% {
+            opacity: 0;
+            transform: translate3d(-50%, 25%, 0px);
+            visibility: visible;
+          }
+          ${1 - 1 * animationOverlap}% {
+            opacity: 1;
+            transform: translate3d(-50%, 75%, 0px);
+          }
+          ${endOfAnimation}% {
+            opacity: 1;
+            transform: translate3d(-50%, 75%, 0px);
+            visibility: visible;
+          }
+          ${endOfAnimation + endOfAnimation * animationOverlap}% {
+            opacity: 0;
+            transform: translate3d(-50%, 135%, 0px);
+            visibility: hidden;
+          }
+          100% {
+            opacity: 0;
+            visibility: visible;
+          }
+        }
+      `}</style>
+    </div>
+  );
+};

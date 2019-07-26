0122ca5809c37f2dbe387f665ab16f4121efb279
Lukáš Huvar <lukas@huvar.cz>
2019-07-26 12:47:42 +0200

    Fix Github button focus (#393)

--- www/components/learn/github-button.js
+++ www/components/learn/github-button.js
@@ -5,9 +5,10 @@ export default ({ login, text, ...props }) => (
   <Button invert color="#24292e" onClick={login} noHover {...props}>
     <style jsx>{`
       .icon {
-        display: inline-block;
+        display: inline-flex;
         vertical-align: middle;
         margin-right: 8px;
+        margin-bottom: 4px;
       }
     `}</style>
     <span className="icon">

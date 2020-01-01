fd87af97ec4e0816a896e90525e39ea8f2c03e16
Luis Alvarez D <luis@zeit.co>
2020-01-01 17:23:02 -0500

    Use a summary twitter card if there's no image (#498)

--- components/social-meta.js
+++ components/social-meta.js
@@ -3,11 +3,11 @@ import Head from 'next/head';
 export default ({ title, description, image, url, keywords }) => (
   <Head>
     <meta name="twitter:site" content="@zeithq" />
+    <meta name="twitter:card" content={image ? 'summary_large_image' : 'summary'} />
     {title && <meta name="og:title" content={title} />}
     {url && <meta name="og:url" content={url} />}
     {description && <meta name="description" content={description} />}
     {description && <meta name="og:description" content={description} />}
-    {image && <meta name="twitter:card" content="summary_large_image" />}
     {image && <meta name="og:image" content={`https://nextjs.org${image}`} />}
     {keywords && <meta name="keywords" content={keywords} />}
   </Head>

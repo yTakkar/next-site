5a0d3905ab4bbb442d4d69d54a1e7f0b064740c6
Shu Uesugi <shu@chibicode.com>
2020-04-18 20:03:58 -0700

    Remove unused code (#634)
    * Remove unused code
    
    * Remove server-side-rendering from withMDX

--- components/enterprise/contact.js
+++
@@ -1,8 +0,0 @@
-import Container from '../container';
-import Button from '../button';
-
-export default () => (
-  <Container center padding>
-    <Button invert>Get in Touch</Button>
-  </Container>
-);
--- next.config.js
+++ next.config.js
@@ -26,7 +26,7 @@ const withGitHubMDX = nextMDX({
 });
 
 const withMDX = nextMDX({
-  extension: /[/\\](pages|blog|telemetry|components[/\\](home|server-side-rendering))[/\\](.+)\.mdx?$/,
+  extension: /[/\\](pages|blog|telemetry|components[/\\](home))[/\\](.+)\.mdx?$/,
   options: {
     hastPlugins: [rehypePrism]
   }

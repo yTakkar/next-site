03ba3a9643c18eb08a3c8623a7bfe877ec124b74
Luis Alvarez D <luis@vercel.com>
2020-05-19 01:34:38 -0500

    Reduced line-height for bigger titles (#703)

--- components/docs/docs-page.js
+++ components/docs/docs-page.js
@@ -116,12 +116,15 @@ function DocsPage({ route, html, prevRoute, nextRoute }) {
         .docs h1 {
           font-size: 3rem;
           font-weight: 700;
+          line-height: 1.35;
         }
         .docs h2 {
           font-size: 2rem;
+          line-height: 1.5;
         }
         .docs h3 {
           font-size: 1.5rem;
+          line-height: 1.6;
         }
         .docs h4 {
           font-size: 1.2rem;
